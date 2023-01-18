b1ac97f08a8796da434ff3b208206ee66f32928f
AbdullahJames <47391931+AbdullahJames@users.noreply.github.com>
2023-01-18 14:54:49 +0000

    changes

--- Week 06 Python 02/fruits_dic.py
+++ Week 06 Python 02/fruits_dic.py
@@ -1,17 +1,31 @@
-def fruit_price(fruit):
-    fruits = {
-        "apple": 0.5,
-        "banana": 0.25,
-        "orange": 0.75
+# Creating a dictionary of three fruits with a corresponding price
+fruits_dict = {
+    "apple": 0.5, 
+    "banana": 0.25, 
+    "orange": 0.75
     }
 
-    if fruit in fruits:
+# Outputting all the fruits in the dictionary
+print(fruits_dict)
+
+# Outputting the price of the second fruit in the dictionary
+print(list(fruits_dict.items())[1][1])
+
+def fruit_price(fruit):
+    if not isinstance(fruit, str):
+        raise TypeError("Input must be a string")
+    # check if the input string is not empty
+    elif not fruit:
+        raise ValueError("Input string must not be empty")
+    # check if the fruit is in the dictionary
+    elif fruit in fruits:
         return fruits[fruit]
     else:
         return f"{fruit} is not in the list."
 
 # Usage examples
-print(fruit_price("apple"))
-print(fruit_price("banana"))
-print(fruit_price("orange"))
-print(fruit_price("pear"))
+print(fruit_price("apple")) # 0.5
+print(fruit_price("banana")) # 0.25
+print(fruit_price("orange")) # 0.75
+print(fruit_price("")) # raise ValueError
+print(fruit_price(1)) # raise TypeError
