b3872f3bba0b2e120893accccf87b64a5f5dc62c
AbdullahJames <47391931+AbdullahJames@users.noreply.github.com>
2023-01-18 15:41:48 +0000

    change

--- Week 06 Python 02/old/fruits_dic.py
+++ Week 06 Python 02/old/fruits_dic.py
@@ -1,28 +1,28 @@
 #Creates a dictionary of three fruits with a corresponding price
-def check_fruit_price(fruit):
-    fruits = {
-        "apple": 0.5,
-        "banana": 0.25,
-        "orange": 0.75
-    }
-    # Output all the fruits in the dictionary
-    print(check_fruit_price.keys())
+fruits = {
+    "apple": 0.5,
+    "banana": 0.25,
+    "orange": 0.75
+}
+
+# Output all the fruits in the dictionary
+print(fruits.keys())
 
 # Output the price of the second fruit in the dictionary
-print(list(check_fruit_price.values())[1])
+print(list(fruits.values())[1])
 
 # For a given fruit, output its price if it is in the dictionary or a message if it is not in the list
 #case where the fruit is in the dictionary
 fruit = "apple"
-if fruit in check_fruit_price:
-    print(check_fruit_price[fruit])
+if fruit in fruits:
+    print(fruits[fruit])
 else:
     print(f"{fruit} is not in the list.")
 
 # For a given fruit, output its price if it is in the dictionary or a message if it is not in the list
 #case where the fruit is not in the dictionary
 fruit = "pear"
-if fruit in check_fruit_price:
-    print(check_fruit_price[fruit])
+if fruit in fruits:
+    print(fruits[fruit])
 else:
     print(f"{fruit} is not in the list.")
